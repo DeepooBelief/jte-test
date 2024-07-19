48e0edc5fe42825f7de663a3699f0a37d3df072f
DeepooBelief <101371088+DeepooBelief@users.noreply.github.com>
2024-07-19 10:06:39 +0100

    Update build.groovy

--- mainfolder/libraries/gradle/steps/build.groovy
+++ mainfolder/libraries/gradle/steps/build.groovy
@@ -1,5 +1,8 @@
 void call(){
     stage("Gradle: Build"){
+        if (config.msg == null) {
+            config.msg = "Hello from Gradle"
+        }
         println "build from the gradle library, msg is: ${config.msg}"
     }
 }
